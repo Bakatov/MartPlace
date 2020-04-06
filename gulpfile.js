6287febd854d5ab3b21966ee8ec1efdbbfc71308
Bakatov <bakatov.vadim@mail.ru>
2020-04-06 23:50:17 +0300

    added header and menu

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ let gulp = require('gulp'),
 
 
 gulp.task('sass', function(){
-     return gulp.src('app/scss/style.scss')
+     return gulp.src('app/scss/**/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(rename({suffix : '.min'}))
     .pipe(autoprefixer({
@@ -21,7 +21,7 @@ gulp.task('sass', function(){
 
 gulp.task('style', function(){
     return gulp.src([
-        'node_modules/normalize.css/normalize.cssg',
+        'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/magnific-popup/dist/magnific-popup.css'
     ])
@@ -60,7 +60,7 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function(){
-    gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+    gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
     gulp.watch('app/*.html', gulp.parallel('html'))
     gulp.watch('app/js/*.js', gulp.parallel('sass'))
 });
